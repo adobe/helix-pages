f919cff9cd664283b64da854f22382588c3f1c33
Alexandre Capt <acapt@adobe.com>
2019-05-09 10:57:52 +0200

    Initial version

---
+++ htdocs/scrani.js
@@ -0,0 +1,86 @@
+(() => {
+    const scrani = (() => {
+    
+        const scrani = { 
+            
+            // config
+            animations: [            
+                {selector: "body>.section", animation:"eager-appear"},
+                {selector: "body>.section>p>img", animation:"wipe"}
+            ],
+
+            scrollY: -1,
+            scrollYBottom:  0,
+        }
+
+        // setup
+        scrani.setup = () => {
+            for (let i=0; i<scrani.animations.length; i++) {
+                a = scrani.animations[i];
+                a.elems=document.querySelectorAll(a.selector);
+            }
+        }
+
+        // update single element
+        scrani.updateElement = (el, animation) => {
+            let progress=0.0;
+            const offsetTop = el.getBoundingClientRect().top+window.pageYOffset;
+
+            if (scrani.scrollY > offsetTop) {
+                progress=1.0;
+            } else if (scrani.scrollYBottom < offsetTop) {
+                progress=0.0;
+            } else {
+                progress=1.0-(offsetTop - scrani.scrollY)/window.innerHeight;
+            }
+
+            // HACK: manually specified animation
+            progress=progress*2;
+            if (progress>1) progress=1;
+            
+            if (animation == "eager-appear") {
+                const transY=100-progress*100;
+                const opacity=progress;
+                el.style=`opacity: ${opacity}; transform: translateY(${transY}px)`;    
+            }
+
+            if (animation == "wipe") {
+                const right=Math.round(100-progress*100);
+                el.style=`clip-path: inset(0 ${right}% 0 0); -webkit-clip-path: inset(0 ${right}% 0 0)`;
+            }
+        }
+
+        // update to get called by requestAnimationFrame
+        scrani.update = (scrollY) => {
+            
+            if (scrollY == scrani.scrollY) return;
+
+            scrani.scrollY = scrollY;
+            scrani.scrollYBottom = scrollY+window.innerHeight;
+
+            for (let i=0; i<scrani.animations.length;i++) {
+                const a = scrani.animations[i];
+                for (let j=0; j<a.elems.length;j++) {
+                    scrani.updateElement(a.elems[j], a.animation);
+                }
+            }
+        }
+
+        //to be called onload
+        scrani.onload = () => {
+            
+            scrani.setup();
+            const repaint = () => {
+                scrani.update(window.scrollY)
+                window.requestAnimationFrame(repaint)
+            }
+            window.requestAnimationFrame(repaint);
+  
+        }
+
+        return (scrani)
+    })();
+
+    window.scrani = scrani; 
+
+})();
