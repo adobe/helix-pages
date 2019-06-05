76fdf253de99e165dfbef42e0a58bb0b88c8637b
Alexandre Capt <acapt@adobe.com>
2019-06-05 16:15:31 +0200

    Added linting + some minor fixes

--- src/html.pre.js
+++ src/html.pre.js
@@ -1,19 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2019 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 const jquery = require('jquery');
 
@@ -23,15 +17,15 @@ const jquery = require('jquery');
  * @param context.content The content
  */
 function pre(context) {
-  const document = context.content.document;
+  const { document } = context.content;
   const $ = jquery(document.defaultView);
 
   /* workaround until sections in document are fixed via PR on pipeline */
   let currentCollection = [];
-  let sections = [];
+  const sections = [];
 
   document.body.childNodes.forEach((child) => {
-    if (child.tagName === "HR") {
+    if (child.tagName === 'HR') {
       sections.push(currentCollection);
       currentCollection = [];
     } else {
@@ -44,8 +38,8 @@ function pre(context) {
     $(el).wrapAll('<div class="section"></div>');
   });
 
-  document.querySelectorAll("body>hr").forEach((el) => {
-    el.parentNode.removeChild(el)
+  document.querySelectorAll('body>hr').forEach((el) => {
+    el.parentNode.removeChild(el);
   });
   /* end of workaround */
 
@@ -54,7 +48,7 @@ function pre(context) {
   // sections with an image
   $sections
     .has(':first-child>img')
-    .addClass('image')
+    .addClass('image');
 
   // first section has a starting image: add title class and wrap all subsequent items inside a div
   $sections
