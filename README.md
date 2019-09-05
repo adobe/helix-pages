72149432ec6a9c8a7ede79e16d27cf0fac4ae033
Tobias Bocanegra <tripod@adobe.com>
2019-09-05 15:02:30 +0900

    feat(content): Support for google docs mounts
    fixes #67

--- README.md
+++ README.md
@@ -21,3 +21,15 @@ The project requires some extensions of the default VCL provided by Helix. The p
 ```
 
 The patched version of the 3 subroutines adds the parsing of the host url to extract the content owner / repo that would override the one stored in the Fastly dictionnary.
+
+
+## Manual testing with google docs support
+
+The google docs support is experimental and requires the `GOOGLE_DOCS_ROOT` action param to be set.
+This is done automatically during CI, but when testing locally it needs to be specified as action param:
+
+eg:
+```
+$ export GOOGLE_DOCS_ROOT=https://script.google.com/macros/s/...scriptId.../exec
+$ hlx up --dev-default GOOGLE_DOCS_ROOT $GOOGLE_DOCS_ROOT
+```
---
+++ src/fetch-google.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const { inspect } = require('util');
+const client = require('request-promise-native');
+const { setdefault } = require('ferrum');
+
+/**
+ * Fetches the Markdown from google docs
+ */
+async function fetch(context, action, resourcePath, rootId) {
+  const { logger, request, secrets } = action;
+  const content = setdefault(context, 'content', {});
+  let timeout;
+  if (!secrets.HTTP_TIMEOUT) {
+    logger.warn('No HTTP timeout set, risk of denial-of-service');
+  } else {
+    timeout = secrets.HTTP_TIMEOUT;
+  }
+
+  // extract some tracing info
+  const {
+    owner, repo, ref,
+  } = request.params;
+  const source = `${owner}/${repo}/${ref}`;
+  const requestId = request.headers['x-request-id'] || '';
+
+  const uri = `${secrets.REPO_RAW_ROOT}?path=${encodeURIComponent(resourcePath)}&rootId=${encodeURIComponent(rootId)}&src=${encodeURIComponent(source)}&rid=${encodeURIComponent(requestId)}`;
+  const options = {
+    uri,
+    json: true,
+    timeout,
+    time: true,
+    followAllRedirects: true,
+  };
+
+  logger.info(`fetching Markdown from ${options.uri}`);
+  try {
+    const resp = await client(options);
+    if (resp.statusCode === 200) {
+      content.body = resp.body;
+    } else {
+      logger.info(`failed loading from google docs: ${resp.statusCode}`);
+      // do not throw error in order to allow resource merging in github repository
+    }
+  } catch (e) {
+    if (e.statusCode === 404) {
+      logger.error(`Could not find Markdown at ${options.uri}`);
+      setdefault(context, 'response', {}).status = 404;
+    } else if ((e.response && e.response.elapsedTime && e.response.elapsedTime > timeout) || (e.cause && e.cause.code && (e.cause.code === 'ESOCKETTIMEDOUT' || e.cause.code === 'ETIMEDOUT'))) {
+      // return gateway timeout
+      logger.error(`Gateway timout of ${timeout} milliseconds exceeded for ${options.uri}`);
+      setdefault(context, 'response', {}).status = 504;
+    } else {
+      logger.error(`Error while fetching Markdown from ${options.uri} with the following `
+        + `options:\n${inspect(options, { depth: null })}`);
+      setdefault(context, 'response', {}).status = 502;
+    }
+    context.error = e;
+  }
+}
+
+module.exports = fetch;
