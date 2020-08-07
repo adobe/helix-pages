1182c2e638fd3fffee6237b462a6e6db3398c6fb
Lars Trieloff <lars@trieloff.net>
2020-08-07 12:07:25 +0000

    test(smoke): add test for raw delivery

--- test/smoke/homepage.smoke.js
+++ test/smoke/homepage.smoke.js
@@ -90,6 +90,21 @@ describe('homepage smoke tests - subdomain extraction and some page content', ()
         });
     });
 
+    it('README gets delivered as Raw', async () => {
+      await chai
+        .request(`https://39430ac97ada5b011835f66e42462b94a3112957--helix-pages--adobe.${argv.domain}`)
+        .get('/adobe/helix-pages/39430ac97ada5b011835f66e42462b94a3112957/README.md')
+        .set('X-Backend-URL', '/adobe/helix-pages/39430ac97ada5b011835f66e42462b94a3112957/README.md')
+        .set('X-Request-Type', 'Static/Redirect')
+        .then((response) => {
+          expect(response).to.be.text;
+          expect(response).to.have.status(200);
+        })
+        .catch((e) => {
+          throw e;
+        });
+    });
+
     it('/etc/passwd does not get delivered', async () => {
       await chai
         .request(`https://master--helix-pages--adobe.${argv.domain}`)
