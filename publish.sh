d3e908f67e02263b562ea9f1da8fe26fe7e33c4d
Tobias Bocanegra <tripod@adobe.com>
2020-12-09 13:17:55 +0900

    update

--- publish.sh
+++ publish.sh
@@ -2,11 +2,6 @@
 
 BRANCH="$(git branch --show-current)"
 
-if [ "$BRANCH" == "master" ]; then
-  echo "publish.sh only needed on feature branch"
-  exit 1
-fi
-
 if [ -n "$(git status --porcelain)" ]; then
   echo "directory not clean."
   exit 1
@@ -14,6 +9,11 @@ fi
 
 echo "publish new version for $BRANCH"
 hlx publish --custom-vcl='vcl/extensions.vcl' --only="$BRANCH" --api-publish=https://adobeioruntime.net/api/v1/web/helix/helix-services/publish@8.0.3-test
+
+if [ "$BRANCH" == "master" ]; then
+  # if on master, we'all done
+  exit 0
+fi
 
 # ensure that helix-config.yaml is update
 if ! git diff --quiet master -- helix-config.yaml; then
