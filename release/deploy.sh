5fc1793c087d5e6422b4d77ab2debc5e6e6ff322
Tobias Bocanegra <tripod@adobe.com>
2021-01-12 17:08:03 +0900

    chore: add missing script

---
+++ release/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Copyright 2021 Adobe. All rights reserved.
+# This file is licensed to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+#
+
+set -v
+
+hlx clean
+hlx build --universal
+hedy -v --deploy --entry-file=.hlx/build/src/html.js       --property.scriptName=html
+hedy -v --deploy --entry-file=.hlx/build/src/embed_html.js --property.scriptName=embed_html
+hedy -v --deploy --entry-file=.hlx/build/src/idx_json.js   --property.scriptName=idx_json
+hedy -v --deploy --entry-file=.hlx/build/src/plain_html.js --property.scriptName=plain_html
+hedy -v --deploy --entry-file=./cgi-bin/feed.js            --property.scriptName=cgi-bin-feed    --test='?src=/en/query-index.json%3Flimit=1&id=path&title=title&updated=date&originalHost=blog.adobe.com'
+hedy -v --deploy --entry-file=./cgi-bin/sitemap.js         --property.scriptName=cgi-bin-sitemap --test='?__hlx_owner=adobe&__hlx_repo=pages&__hlx_ref=master'
+
+# update package secrets
+if [[ -f ".pages-package.env" ]]; then
+  hedy -v --no-build --no-hints --update-package --no-test
+fi
+
+# update helix-config.yaml
+node ./update-config.js
