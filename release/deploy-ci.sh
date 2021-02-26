d92cf4e4a083dfb6101a6db102a4c88f08e04a76
Lars Trieloff <trieloff@adobe.com>
2021-02-26 14:32:47 +0100

    build(deploy): update all universal strains when releasing

--- release/deploy-ci.sh
+++ release/deploy-ci.sh
@@ -30,4 +30,4 @@ if [[ -f ".pages-package.env" ]]; then
 fi
 
 # update helix-config.yaml
-node ./release/update-config.js --pkgVersion ci$CIRCLE_BUILD_NUM --strain default --strain universal
+node ./release/update-config.js --pkgVersion ci$CIRCLE_BUILD_NUM --strain default --strain universal --strain universal-ci
