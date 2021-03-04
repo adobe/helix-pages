447e1dc6dcf9d76e251dd0ad10b3b31c81418e83
Tobias Bocanegra <tripod@adobe.com>
2021-03-04 19:09:58 +0900

    feat(blobs): adjust to rootless image paths

--- src/create-pictures.js
+++ src/create-pictures.js
@@ -17,7 +17,7 @@ async function createPictures({ content }) {
   const { document } = content;
 
   // transform <img> to <picture>
-  document.querySelectorAll('img[src^="/hlx_"]').forEach((img, i) => {
+  document.querySelectorAll('img[src^="./media_"]').forEach((img, i) => {
     const picture = document.createElement('picture');
     const source = document.createElement('source');
     source.setAttribute('media', '(max-width: 400px)');
