65dde417cd5d836783ecfb439914ef1526a49917
rofe <github@rofe.com>
2021-03-22 23:41:41 +0100

    refactor: simplify title and hero extraction

--- src/extract-metadata.js
+++ src/extract-metadata.js
@@ -171,12 +171,7 @@ async function extractMetaData(context, action) {
 
   // content.title is not correct if the h1 is in a page-block since the pipeline
   // only respects the heading nodes in the mdast
-  let $title;
-  document.querySelectorAll('body > div').forEach(($section) => {
-    if (!$title) {
-      $title = $section.querySelector('h1');
-    }
-  });
+  const $title = document.querySelector('body > div h1');
   if ($title) {
     content.title = $title.textContent;
   }
@@ -200,12 +195,7 @@ async function extractMetaData(context, action) {
 
   // content.image is not correct if the first image is in a page-block. since the pipeline
   // only respects the image nodes in the mdast
-  let $hero;
-  document.querySelectorAll('body > div').forEach(($section) => {
-    if (!$hero) {
-      $hero = $section.querySelector('img');
-    }
-  });
+  const $hero = document.querySelector('body > div img');
   if ($hero) {
     content.image = $hero.src;
   }
