049127683ff30188ccae73078ac8420b96ed0c0b
Tobias Bocanegra <tripod@adobe.com>
2021-03-24 12:46:16 +0900

    fix(picture): use backward compatible format

--- test/unit/rendering.test.js
+++ test/unit/rendering.test.js
@@ -152,8 +152,8 @@ describe('Rendering', () => {
       const imgs = pics.map((pic) => pic.querySelector('img'));
       assert.strictEqual(pics.length, 3, 'document has 3 pictures');
       assert.strictEqual(imgs.length, 3, 'document has 3 images');
-      assert.ok(pics.every((pic) => pic.querySelector('source').getAttribute('srcset').endsWith('?width=750&format=webply&optimize=medium')), 'pictures have source sets with correct parameters');
-      assert.ok(imgs.every((img) => img.getAttribute('src').endsWith('?width=2000&format=webply&optimize=medium')), 'images have source with correct parameters');
+      assert.ok(pics.every((pic) => pic.querySelector('source').getAttribute('srcset').endsWith('?width=750&auto=webp&format=pjpg&optimize=medium')), 'pictures have source sets with correct parameters');
+      assert.ok(imgs.every((img) => img.getAttribute('src').endsWith('?width=2000&auto=webp&format=pjpg&optimize=medium')), 'images have source with correct parameters');
       assert.ok(imgs.shift().getAttribute('loading') === 'eager', 'first image has loading set to eager');
       assert.ok(imgs.every((img) => img.getAttribute('loading') === 'lazy'), 'all other images have loading set to lazy');
     });
@@ -165,8 +165,8 @@ describe('Rendering', () => {
       const imgs = pics.map((pic) => pic.querySelector('img'));
       assert.strictEqual(pics.length, 3, 'document has 3 pictures');
       assert.strictEqual(imgs.length, 3, 'document has 3 images');
-      assert.ok(pics.every((pic) => pic.querySelector('source').getAttribute('srcset').endsWith('?width=750&format=webply&optimize=medium')), 'pictures have source sets with correct parameters');
-      assert.ok(imgs.every((img) => img.getAttribute('src').endsWith('?width=2000&format=webply&optimize=medium')), 'images have source with correct parameters');
+      assert.ok(pics.every((pic) => pic.querySelector('source').getAttribute('srcset').endsWith('?width=750&auto=webp&format=pjpg&optimize=medium')), 'pictures have source sets with correct parameters');
+      assert.ok(imgs.every((img) => img.getAttribute('src').endsWith('?width=2000&auto=webp&format=pjpg&optimize=medium')), 'images have source with correct parameters');
       assert.ok(imgs.shift().getAttribute('loading') === 'eager', 'first image has loading set to eager');
       assert.ok(imgs.every((img) => img.getAttribute('loading') === 'lazy'), 'all other images have loading set to lazy');
     });
