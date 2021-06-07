3bb497ecbf39f6757e23c5318d182b38421877bc
Lars Trieloff <trieloff@adobe.com>
2021-06-07 10:14:44 +0200

    fix(utils): live domain can be anywhere in the xfh
    as pointed out by @stefan-guggisberg in https://github.com/adobe/helix-pages/commit/d81af799db6c0151017568e3a6bdfa7482fd0032#r51813211

--- assembly/handlers/utils.ts
+++ assembly/handlers/utils.ts
@@ -3,7 +3,7 @@ import { Request } from "@fastly/as-compute";
 export function contentBusPartition(req: Request) {
   let partition = "preview";
   let xfh = req.headers.get("x-forwarded-host");
-  if (xfh != null && xfh.endsWith(".live")) {
+  if (xfh != null && (xfh.endsWith(".live") || xfh.includes(".live,"))) {
     partition = "live";
   }
 
