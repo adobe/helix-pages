3ced1e58492929fce61f6ca9a4f85ca5e0810be2
Lars Trieloff <trieloff@adobe.com>
2021-06-11 14:17:02 +0200

    fix(utils): fix content bus partition determination

--- assembly/handlers/utils.ts
+++ assembly/handlers/utils.ts
@@ -1,9 +1,9 @@
 import { Request } from "@fastly/as-compute";
 
-export function contentBusPartition(req: Request) {
+export function contentBusPartition(req: Request): string {
   let partition = "preview";
   let xfh = req.headers.get("x-forwarded-host");
-  if (xfh != null && (xfh.endsWith(".live") || xfh.includes(".live,"))) {
+  if (xfh != null && ((xfh as string).endsWith(".live") || (xfh as string).includes(".live,"))) {
     partition = "live";
   }
 
